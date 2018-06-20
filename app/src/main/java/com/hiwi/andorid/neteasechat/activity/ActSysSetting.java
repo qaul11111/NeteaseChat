88e8c50ea7f14eb853822ffbdec079248aadfbdb
weikaino1 <343565682@qq.com>
2018-06-20 10:00:59 +0800

    讨论组设置

---
+++ app/src/main/java/com/hiwi/andorid/neteasechat/activity/ActSysSetting.java
@@ -0,0 +1,16 @@
+package com.hiwi.andorid.neteasechat.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.hiwi.andorid.neteasechat.R;
+
+public class ActSysSetting extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_sys_setting);
+    }
+}
